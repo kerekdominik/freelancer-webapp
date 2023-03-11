cf4e32081ef41ed9d025df25751abb3dbd09312e
Dominik <kerek.dominik06@gmail.com>
2023-03-11 13:31:19 +0100

    Init

---
+++ src/test/java/bme/webapp/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package bme.webapp.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
