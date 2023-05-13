f0f47b03468a68e517be0af62420c6313e8721cf
Dominik <kerek.dominik06@gmail.com>
2023-05-13 16:56:53 +0200

    Cross-origin config

--- backend/src/main/java/bme/webapp/freelancer/repository/JobRepository.java
+++ backend/src/main/java/bme/webapp/freelancer/repository/JobRepository.java
@@ -2,8 +2,6 @@ package bme.webapp.freelancer.repository;
 
 import bme.webapp.freelancer.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
 public interface JobRepository extends JpaRepository<Job, Integer> {
 }
